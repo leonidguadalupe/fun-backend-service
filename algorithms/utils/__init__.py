f25fc3f50dd5060e82ec5ee22b9ee41606a5101c
Leonid Adolph Guadalupe <leonidguadalupe@gmail.com>
2020-01-17 18:34:08 +0800

    Adding quick sort algorithm and import updates for utils folder

---
+++ algorithms/utils/__init__.py
@@ -0,0 +1,4 @@
+from .sort import Sort # noqa: F401
+from .quick import QuickSort # noqa: F401
+
+__all__ = ["QuickSort"]
--- algorithms/utils/quick.py
+++ algorithms/utils/quick.py
@@ -1,15 +1,58 @@
+import copy
 import json
 from .sort import Sort
+        
 
 class QuickSort(Sort):
     def __init__(self, array):
         super().__init__(array)
     
-    # My own algorithm for quick sort
+    def _swap(self: object, array: list, left: int, right: int) -> list:
+        array[left], array[right] = array[right], array[left]
+        return array
+
+    def _doQuickSort(self: object, array: list, pivot: int, left: int, right: int) -> None:
+        if left >= right:
+            return
+        while right >= left:
+            if array[pivot] < array[left] and array[pivot] > array[right]:
+                self._swap(array, left, right)
+                self.result.append({
+                    "list": copy.deepcopy(array),
+                    "right": right,
+                    "left": left,
+                    "pivot": pivot
+                })
+                continue #cut here to show swap change in frontend
+            if array[pivot] >= array[left]:
+                left += 1
+            if array[pivot] <= array[right]:
+                right -= 1
+            self.result.append({
+                    "list": copy.deepcopy(array),
+                    "right": right,
+                    "left": left,
+                    "pivot": pivot
+            })
+        # swap pivot with right
+        self._swap(array, pivot, right)
+        self.result.append({
+                "list": copy.deepcopy(array),
+                "right": right,
+                "left": left,
+                "pivot": pivot
+        })
+        # divide and conquer
+        self._doQuickSort(array, pivot, pivot+1, right-1)
+        self._doQuickSort(array, left, left+1, len(array)-1)
+        
+        return 
+    
     def sort(self):
-        pass
+        self._doQuickSort(self.array, 0, 1, len(self.array)-1)
+        return self.result
     
     # Let's sort and serialize boiz
-    def serialize(self):
+    def serialize(self) -> str:
         self.result = self.sort()
         return json.dumps(self.result)
